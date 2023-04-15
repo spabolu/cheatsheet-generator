18d9ebdc2c45bdf70185cee43931a82dec530b62
Saketh Pabolu <51687055+spabolu@users.noreply.github.com>
2023-04-14 18:29:13 -0700

    made the components render conditionally

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent } from 'react';
+import React, { ReactNode, useState, ChangeEvent } from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import remarkMath from 'remark-math';
@@ -7,20 +7,74 @@ import rehypeKatex from 'rehype-katex';
 import './App.css';
 
 const App: React.FC = () => {
-  const [columnOne, setColumnOne] = useState<string>('');
-  const [columnTwo, setColumnTwo] = useState<string>('');
-  const [columnThree, setColumnThree] = useState<string>('');
+  const [columns, setColumns] = useState<string[]>(['', '', '']);
 
-  const handleColumnOneChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setColumnOne(e.target.value);
+  interface MarkdownComponentProps {
+    node: ReactNode;
+    [key: string]: any;
+  }
+
+  const markdownComponents: {
+    [key: string]: React.FC<MarkdownComponentProps>;
+  } = {
+    h1: ({ node, ...props }: MarkdownComponentProps) => <h1 className="text-2xl" {...props} />,
+    h2: ({ node, ...props }: MarkdownComponentProps) => <h2 className="text-xl" {...props} />,
+    h3: ({ node, ...props }: MarkdownComponentProps) => <h3 className="text-lg" {...props} />,
+    h4: ({ node, ...props }: MarkdownComponentProps) => <h4 className="text-base" {...props} />,
+    h5: ({ node, ...props }: MarkdownComponentProps) => <h5 className="text-sm" {...props} />,
+    h6: ({ node, ...props }: MarkdownComponentProps) => <h6 className="text-xs" {...props} />,
+    p: ({ node, ...props }: MarkdownComponentProps) => <p className="text-base" {...props} />,
+    ul: ({ node, ...props }: MarkdownComponentProps) => <ul className="list-disc" {...props} />,
+    ol: ({ node, ...props }: MarkdownComponentProps) => <ol className="list-decimal" {...props} />,
+    li: ({ node, ...props }: MarkdownComponentProps) => <li className="text-base" {...props} />,
+    blockquote: ({ node, ...props }: MarkdownComponentProps) => (
+      <blockquote className="border-l-4 border-gray-300 pl-4" {...props} />
+    ),
+    table: ({ node, ...props }: MarkdownComponentProps) => (
+      <table className="table-auto border-collapse border border-gray-300" {...props} />
+    ),
+    thead: ({ node, ...props }: MarkdownComponentProps) => (
+      <thead className="border border-gray-300" {...props} />
+    ),
+    tbody: ({ node, ...props }: MarkdownComponentProps) => (
+      <tbody className="border border-gray-300" {...props} />
+    ),
+    tr: ({ node, ...props }: MarkdownComponentProps) => (
+      <tr className="border border-gray-300" {...props} />
+    ),
+    th: ({ node, ...props }: MarkdownComponentProps) => (
+      <th className="border border-gray-300" {...props} />
+    ),
+    td: ({ node, ...props }: MarkdownComponentProps) => (
+      <td className="border border-gray-300" {...props} />
+    ),
+    pre: ({ node, ...props }: MarkdownComponentProps) => (
+      <pre className="bg-gray-100 p-2" {...props} />
+    ),
+    code: ({ node, ...props }: MarkdownComponentProps) => (
+      <code className="rounded-md bg-gray-100 p-0.5" {...props} />
+    ),
+    inlineCode: ({ node, ...props }: MarkdownComponentProps) => (
+      <code className="rounded-md bg-gray-100 p-0.5" {...props} />
+    ),
+    del: ({ node, ...props }: MarkdownComponentProps) => (
+      <del className="line-through" {...props} />
+    ),
+    em: ({ node, ...props }: MarkdownComponentProps) => <em className="italic" {...props} />,
+    strong: ({ node, ...props }: MarkdownComponentProps) => (
+      <strong className="font-bold" {...props} />
+    ),
+    hr: ({ node, ...props }: MarkdownComponentProps) => (
+      <hr className="border-gray-300" {...props} />
+    ),
+    a: ({ node, ...props }: MarkdownComponentProps) => <a className="text-blue-600" {...props} />,
+    img: ({ node, ...props }: MarkdownComponentProps) => <img className="max-w-full" {...props} />,
   };
 
-  const handleColumnTwoChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setColumnTwo(e.target.value);
-  };
-
-  const handleColumnThreeChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setColumnThree(e.target.value);
+  const handleColumnChange = (e: ChangeEvent<HTMLTextAreaElement>, columnIndex: number) => {
+    const newColumns = [...columns];
+    newColumns[columnIndex] = e.target.value;
+    setColumns(newColumns);
   };
 
   const generatePDF = () => {
@@ -28,49 +82,32 @@ const App: React.FC = () => {
   };
 
   return (
-    <main className="flex flex-row items-stretch w-full">
-      <div className="w-1/2 h-screen flex flex-col">
-        <textarea
-          className="h-1/3 border-2 border-red-300 m-2 p-2"
-          onChange={handleColumnOneChange}
-        />
-
-        <textarea
-          className="h-1/3 border-2 border-red-300 m-2 p-2"
-          onChange={handleColumnTwoChange}
-        />
-
-        <textarea
-          className="h-1/3 border-2 border-red-300 m-2 p-2"
-          onChange={handleColumnThreeChange}
-        />
+    <main className="flex w-full flex-row items-stretch">
+      <div className="flex h-screen w-1/2 flex-col">
+        {columns.map((_, index) => (
+          <textarea
+            key={index}
+            className="m-2 h-1/3 border-2 border-red-300 p-2"
+            onChange={(e) => handleColumnChange(e, index)}
+          />
+        ))}
       </div>
 
-      <div className="w-1/2 h-screen flex flex-col">
-        <div className="h-full border-2 my-2 border-cyan-600 flex flex-row">
-          <ReactMarkdown
-            children={columnOne}
-            remarkPlugins={[remarkMath, remarkGfm]}
-            rehypePlugins={[rehypeKatex]}
-            className="p-2 w-1/3 h-full overflow-auto"
-          />
-
-          <ReactMarkdown
-            children={columnTwo}
-            remarkPlugins={[remarkMath, remarkGfm]}
-            rehypePlugins={[rehypeKatex]}
-            className="p-2 w-1/3 h-full overflow-auto"
-          />
-
-          <ReactMarkdown
-            children={columnThree}
-            remarkPlugins={[remarkMath, remarkGfm]}
-            rehypePlugins={[rehypeKatex]}
-            className="p-2 w-1/3 h-full overflow-auto"
-          />
+      <div className="flex h-screen w-1/2 flex-col">
+        <div className="my-2 flex h-full flex-row border-2 border-indigo-300">
+          {columns.map((column, index) => (
+            <ReactMarkdown
+              key={index}
+              children={column}
+              remarkPlugins={[remarkMath, remarkGfm]}
+              rehypePlugins={[rehypeKatex]}
+              className="h-full w-1/3 overflow-auto hyphens-auto p-2"
+              components={markdownComponents}
+            />
+          ))}
         </div>
         <button
-          className="my-2 py-2 px-4 bg-cyan-500 hover:bg-cyan-700 text-white font-bold border-2 rounded-md border-cyan-500 hover:border-cyan-700"
+          className="my-2 rounded-md border-2 border-indigo-500 bg-indigo-500 px-4 py-2 font-bold text-white hover:border-indigo-700 hover:bg-indigo-700"
           onClick={generatePDF}
         >
           Generate PDF!
